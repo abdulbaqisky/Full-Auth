1b4a8bdcb024f038fc3c7455683893064ff6a7f9
Abdulbaqi <adekomiabdulbaqi3@gmail.com>
2025-01-07 09:51:34 +0100

    ejs good

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ import express from 'express'
 import connectMongoDB from './db.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 connectMongoDB()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
-});
+    res.render('index', {title: 'Home Page'})
+})
+
+app.get('/login', (req, res) => {
+    res.render('login', {title: 'Login Page'})
+})
 
 app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
     
 })
-
- 
